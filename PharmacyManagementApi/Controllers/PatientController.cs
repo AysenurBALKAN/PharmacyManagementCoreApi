68e06abab934bb649ed378cbcdf7348ee59e6e7b
Ayşenur Balkan <balkan.aysenur@gmail.com>
2024-04-24 15:21:15 +0300

    Add files via upload

---
+++ PharmacyManagementApi/Controllers/PatientController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PBusinessLayer.Concrete;
+using PDataAccessLayer.Concrete;
+using PDataAccessLayer.EntityFramework;
+using PEntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PharmacyManagementApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PatientController : ControllerBase
+    {
+        PatientManager cm = new PatientManager(new EfPatient());
+
+        [HttpGet]
+        public IActionResult PatientList()
+        {
+            var values = cm.TGetList();
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public IActionResult PatientAdd(PatientInfo t)
+        {
+            cm.TAdd(t);
+            return Ok();
+        }
+
+        [HttpDelete ]
+        public IActionResult PatientDelete(PatientInfo t)
+        {
+             cm.TDelete(t);
+            return Ok();
+        }
+
+       [HttpPut]
+        public IActionResult PatientUpdate(PatientInfo t)
+        {
+            cm.TUpdate(t);
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult PatientList(int id)
+        {
+            var values = cm.TGetById(id);
+            return Ok(values);
+        }
+    }
+}
